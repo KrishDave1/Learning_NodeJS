e3f6efee940f4c84aa0bfca5b0a20be7b338c4a5
KrishDave1 <Krish.Dave@iiitb.ac.in>
2023-12-27 16:02:47 +0530

    file handling understood and async and sync file methods understood

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -2,6 +2,7 @@
 
 const { readFileSync, writeFileSync, write } = require("fs"); //Method 1;
 
+console.log('start');
 const first = readFileSync("./content/first.txt", "utf-8");
 const second = readFileSync("./content/second.txt", "utf-8");
 
@@ -10,3 +11,6 @@ writeFileSync(
   `Here is the final result : ${first}, ${second}`,
   { flag: "a" } // This flag is for indicating append mode
 );
+
+console.log('done with this task');
+console.log('starting the next one');
--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-/** @format */
+const http = require('http');
 
-const { readFile, writeFile } = require("fs"); //Method 1;
-
-readFile('./content/first.txt','utf-8', (err, result) => {
-    if (err) {
-        console.log(err);
-        return
-    }
-    console.log(result)
+const server = http.createServer((req, res) => {
+    res.write('Welcome to our home page');
+    res.end();
 })
 
-const first = result;
-readFile('./content/second.txt', 'utf-8', (err, result) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    const second = result;
-    writeFile('./content/second.txt')
-})
+server.listen(5000);
