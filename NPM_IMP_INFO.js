18d8e65664810f6c471ddad5d5b80c8fbab9f050
KrishDave1 <Krish.Dave@iiitb.ac.in>
2023-12-29 01:27:18 +0530

    http server basics

---
+++ NPM_IMP_INFO.js
@@ -0,0 +1,6 @@
+//local dependency - use it only in this particular project
+//npm i <packageName>
+
+//global dependency - use it in any project
+//npm install -g <packageName>
+//sudo npm install -g <packageName> (mac)
