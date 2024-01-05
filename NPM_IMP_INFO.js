aef77906b2463bec1261e9b19fc501e5899544c7
KrishDave1 <Krish.Dave@iiitb.ac.in>
2024-01-06 01:19:34 +0530

    npm general information

--- NPM_IMP_INFO.js
+++ NPM_IMP_INFO.js
@@ -4,3 +4,7 @@
 //global dependency - use it in any project
 //npm install -g <packageName>
 //sudo npm install -g <packageName> (mac)
+
+//package.json - manifest file (stores important info about project/package)
+//In terminal type npm init to create package.json file
+//npm init -y to create package.json file with default values.
